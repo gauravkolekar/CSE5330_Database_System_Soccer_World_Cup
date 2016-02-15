b09555e16be94218a3e6f904d8ced33d3394993e
Gaurav Kolekar <gaurav.kolekar@mavs.uta.edu>
2016-02-15 01:07:25 -0600

    connection parameter to connect to the database added

--- soccer.py
+++ soccer.py
@@ -4,3 +4,14 @@ Team:
 Gaurav Vivek Kolekar
 Amit Hegde
 '''
+import MySQLdb
+#Importing MySQLdb library so that my python program can talk to the MYSQL database
+db = MySQLdb.connect(host="127.0.0.1", port=3306, user="root", passwd="password", db="SOCCER")
+#connecting to the database
+cur = db.cursor()
+#creating a cursor object to excute queries on the database
+cur.execute("SELECT VERSION()")
+data = cur.fetchone()
+print "Database version:", str(data)
+db.close()
+#closing database connection
