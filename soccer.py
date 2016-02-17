1d32979de4833032f34242464f9669384fdb719a
Gaurav Kolekar <gaurav.kolekar@mavs.uta.edu>
2016-02-16 18:31:14 -0600

    added foreign keys to all the tables

--- soccer.py
+++ soccer.py
@@ -24,7 +24,7 @@ data = cur.fetchone()
 print "Database version: ",str(data)
 #printing the database version
 
-cur.execute("DROP TABLE IF EXISTS COUNTRY,PLAYERS,MATCH_RESULTS,PLAYER_CARD,PLAYER_ASSIST_GOALS")
+cur.execute("DROP TABLE IF EXISTS PLAYER_ASSIST_GOALS,PLAYER_CARD,MATCH_RESULTS,PLAYERS,COUNTRY;")
 #dropping table if they exsist to start fresh
 
 create_country_table_sql = """CREATE TABLE COUNTRY(
@@ -47,9 +47,69 @@ create_player_table_sql = """CREATE TABLE PLAYERS(
                              Club VARCHAR(20),
                              Position VARCHAR(10),
                              Caps_for_Country INTEGER,
-                             IS_CAPTAIN BOOLEAN);"""
+                             IS_CAPTAIN BOOLEAN,
+                             PRIMARY KEY(Player_id));"""
 cur.execute(create_player_table_sql)
 #run the player table sql query
+
+create_match_results_table_sql = """CREATE TABLE MATCH_RESULTS(
+                                    Match_id INTEGER NOT NULL,
+                                    Date_of_Match DATE,
+                                    Start_Time_Of_Match TIME,
+                                    Team1 VARCHAR(25),
+                                    Team2 VARCHAR(25),
+                                    Team1_score INTEGER,
+                                    Team2_score INTEGER,
+                                    Stadium_Name VARCHAR(35),
+                                    Host_City VARCHAR(20),
+                                    PRIMARY KEY(Match_id));"""
+cur.execute(create_match_results_table_sql)
+#run the match_result sql query
+
+create_player_card_table_sql = """CREATE TABLE PLAYER_CARD(
+                                  Player_id INTEGER NOT NULL,
+                                  Yellow_Card INTEGER,
+                                  Red_Cards INTEGER,
+                                  PRIMARY KEY(Player_id));"""
+cur.execute(create_player_card_table_sql)
+#run the player_card sql query
+
+create_player_assists_goals = """CREATE TABLE PLAYER_ASSIST_GOALS(
+                                 Player_id INTEGER NOT NULL,
+                                 No_of_Matches INTEGER,
+                                 Goals INTEGER,
+                                 Assists INTEGER,
+                                 PRIMARY KEY(Player_id));"""
+cur.execute(create_player_assists_goals)
+#run the player_assist_goals sql qery
+
+add_foreign_Players_sql = """ALTER TABLE PLAYERS
+                             ADD FOREIGN KEY(Country)
+                             REFERENCES COUNTRY(Country_Name);"""
+cur.execute(add_foreign_Players_sql)
+#adding foreign key to player
+
+add_foreign_match_result_sql = """ALTER TABLE MATCH_RESULTS
+                                  ADD FOREIGN KEY (Team1)
+                                  REFERENCES COUNTRY(Country_Name),
+                                  ADD FOREIGN KEY (Team2)
+                                  REFERENCES COUNTRY(Country_Name);"""
+cur.execute(add_foreign_match_result_sql)
+#adding the foreign key to match result table
+
+add_foreign_key_player_card_sql = """ALTER TABLE PLAYER_CARD
+                                     ADD FOREIGN KEY (Player_id)
+                                     REFERENCES PLAYERS(Player_id);"""
+cur.execute(add_foreign_key_player_card_sql)
+#adding the foriegn key to player_card table
+
+add_foreign_key_player_assist_goals_sql = """ALTER TABLE PLAYER_ASSIST_GOALS
+                                             ADD FOREIGN KEY (Player_id)
+                                             REFERENCES PLAYERS(Player_id);"""
+cur.execute(add_foreign_key_player_assist_goals_sql)
+#adding the foriegn key to PLAYER_ASSIST_GOALS table
+
+
 
 db.close()
 #closing database connection
